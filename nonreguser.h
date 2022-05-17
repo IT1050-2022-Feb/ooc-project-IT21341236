6deef07b6bb6d9a2cb0f430614d9e2039c809f8e
IT21341236 <99305720+IT21341236@users.noreply.github.com>
2022-05-17 11:02:14 +0000

    nonreguser.h nonreguser.cpp reguser.h reguser.cpp

---
+++ nonreguser.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class nonRegUser {
+protected:
+    int regid;
+    char regname[20];
+    char regaddress[50];
+    char regemail[30];
+    int regno;
+public:
+    nonRegUser();
+    nonRegUser(int rid, char rname[], char raddress[], char remail[], int rno);
+    void browsepackages();
+    void registersystem();
+    ~nonRegUser();
+
+};
---
+++ reguser.h
@@ -0,0 +1,18 @@
+#include <cstring>
+#include “database.h”
+
+class RegUser :public nonRegUser {
+private:
+    DataBase* database;
+    char regUsername[20];
+    char regPassword[20];
+public:
+    RegUser();
+    RegUser(char rusername[], char rpassword[]);
+    void login();
+    void logout();
+    void updateprofile();
+    ~RegUser();
+
+};
+
